a595f764369170763063a445bff627691fd0b774
khalid102030 <64660179+khalid102030@users.noreply.github.com>
2026-03-19 07:19:58 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,11 @@
-const CACHE = 'dropp-driver-v1';
-const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
+const CACHE = 'dropp-driver-v2';
+const ASSETS = [
+  '/dropp-driver/',
+  '/dropp-driver/index.html',
+  '/dropp-driver/manifest.json',
+  '/dropp-driver/icon-192.png',
+  '/dropp-driver/icon-512.png'
+];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
@@ -15,6 +21,7 @@ self.addEventListener('activate', e => {
 
 self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(e.request).then(cached => cached || fetch(e.request).catch(() => caches.match('./index.html')))
+    caches.match(e.request).then(cached => cached || fetch(e.request)
+      .catch(() => caches.match('/dropp-driver/index.html')))
   );
 });
